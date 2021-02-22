0f2108a1b66b408f280472df70ebc113ef0abdf5
Sean Slater <sean@slaterfam.com>
2021-02-22 17:38:46 -0500

    lists proper output and material generator

--- spats_frontend/display_generator.py
+++ spats_frontend/display_generator.py
@@ -4,10 +4,10 @@ class DisplayGenerator:
 	def __init__(self):
 		pass
 
-	def _get_thing(self, thing, asset):
-		fields = thing['fields']
-		primary = asset.get('primary')
-		order = asset.get('order')
+	def _single_material(self, material, symbolic):
+		fields = material['fields']
+		primary = symbolic.get('primary')
+		order = symbolic.get('order')
 		output = []
 		for key in order:
 			if key != primary and key in fields:
@@ -15,23 +15,54 @@ class DisplayGenerator:
 				val = raw if (not isinstance(raw, list)) else ', '.join([str(r) for r in raw])
 				output.append((key, val))
 		return {
-			'_id': thing['_id'],
+			'_id': material['_id'],
 			'primary': fields[primary],
-			'type': thing['type'],
-			'asset': asset['name'],
+			'type': material['type'],
+			'symbolic': symbolic['name'],
 			'fields': output
+		}
+
+	def _many_material(self, material, symbolic):
+		fields = material['fields']
+		primary = None
+		secondary = None
+		tertiary = []
+		primary_field = symbolic.get('primary')
+		secondary_field = symbolic.get('secondary')
+		tertiary_fields = symbolic.get('tertiary', [])
+		if primary_field:
+			value = fields.get(primary_field)
+			if value:
+				primary = (primary_field, value)
+		if secondary_field:
+			value = fields.get(secondary_field)
+			if value:
+				secondary = (secondary_field, value)
+		if tertiary_fields:
+			for tertiary_field in tertiary_fields:
+				value = fields.get(tertiary_field)
+				if value:
+					tertiary.append((tertiary_field, value))
+
+		return {
+			'_id': material['_id'],
+			'primary': primary,
+			'secondary': secondary,
+			'tertiary': tertiary,
+			'type': material['type'],
+			'symbolic': symbolic['name']
 		}
 
 	def thing_get(self, doc):
 		thing = doc['thing']
 		asset = doc['asset'][thing['type']]
-		return self._get_thing(thing, asset)
+		return self._single_material(thing, asset)
 
 	def thing_get_all(self, doc):
 		things = doc['thing']
-		asset = doc['asset'][things[0]['type']]
 		res = []
 		for thing in things:
-			cur = self._get_thing(thing, asset)
+			asset = doc['asset'][thing['type']]
+			cur = self._many_material(thing, asset)
 			res.append(cur)
 		return res
